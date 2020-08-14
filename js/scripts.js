615b1cce7538b93e163acaa2c288d544306aa2ea
Alex Goodwin <hagoodwi@gmail.com>
2020-08-14 12:54:00 -0700

    add branching in scripts

--- js/scripts.js
+++ js/scripts.js
@@ -8,10 +8,14 @@ $(document).ready(function() {
     const area = $("input:radio[name=area]:checked").val();
     const weather = $("input:radio[name=weather]:checked").val();
 
-    if (subject === "object-oriented" && punctuation === ";") {
+    if (subject === "object-oriented" && punctuation === ";" && color === "blue" && area === "backend" && weather === "rain") {
       $("#output").text("Your suggested language is Java!");
-    } else if(color === "green") {
+    } else if (color === "green") {
       $("#output").text("Your suggested language is Ruby!");
+    } else if (subject === "object-oriented" && area === "backend") {
+      $("#output").text("Your suggested language is Javascript!");
+    } else if (color === "red") {
+      $("#output").text("Pick whichever you like! We aren't sure for you!");
     } else {
       alert("error");
     }
