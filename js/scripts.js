4564b72e6b1678a296e382a19bdf3f4042a9bc28
Alex Goodwin <hagoodwi@gmail.com>
2020-08-14 12:02:08 -0700

    create first successful output text

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,24 @@
 $(document).ready(function() {
+  console.log("Script executing!");
   $("#form0ne").submit(function(event){
     event.preventDefault();
     const subject = $("input:radio[name=subject]:checked").val();
+    const punctuation = $("input:radio[name=punctuation]:checked").val();
+    const color = $("input:radio[name=color]:checked").val();
+    const area = $("input:radio[name=area]:checked").val();
+    const weather = $("input:radio[name=weather]:checked").val();
 
-    $("#output").text(subject);
+    if (subject === "object-oriented" && punctuation === ";") {
+      $("#output").text("Your suggested language is Java!");
+    } else if(color === "green") {
+      $("#output").text("Your suggested language is Ruby!");
+    } else {
+      alert("error");
+    }
 
-    // $("#story").show();
+
+    //$("#output").text(subject);
+
+    
   });
 });
